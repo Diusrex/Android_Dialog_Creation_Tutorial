1231f2f3b284491464ee2d1c9f10c3a99aba3d6d
Diusrex <killakan002@hotmail.com>
2015-01-09 14:21:50 -0700

    Set up the project for Android Studio

---
+++ app/src/main/java/com/diusrex/dialogtutorial/DialogTutorial.java
@@ -0,0 +1,13 @@
+package com.diusrex.dialogtutorial;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DialogTutorial extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dialog_tutorial);
+    }
+}
