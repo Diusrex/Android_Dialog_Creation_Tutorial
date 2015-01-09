381127f45b43498ce8769eb7e481c4a7fd25390f
Diusrex <killakan002@hotmail.com>
2015-01-09 14:31:30 -0700

    Set up the OnCreateView DialogFragment example.

--- app/src/main/java/com/diusrex/dialogtutorial/DialogTutorial.java
+++ app/src/main/java/com/diusrex/dialogtutorial/DialogTutorial.java
@@ -1,13 +1,24 @@
 package com.diusrex.dialogtutorial;
 
 import android.app.Activity;
+import android.app.DialogFragment;
 import android.os.Bundle;
+import android.view.View;
 
 public class DialogTutorial extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dialog_tutorial);
+    }
+
+    // Button onClick functions
+    public void displayOnCreateViewDialogFragment(View v) {
+        OnCreateViewDialogFragment fragment = OnCreateViewDialogFragment.newInstance("On Create View DialogFragment");
+        showDialogFragment(fragment);
+    }
+
+    private void showDialogFragment(DialogFragment fragment) {
+        fragment.show(getFragmentManager(), "dialog");
     }
 }
---
+++ app/src/main/java/com/diusrex/dialogtutorial/OnCreateViewDialogFragment.java
@@ -0,0 +1,65 @@
+package com.diusrex.dialogtutorial;
+
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class OnCreateViewDialogFragment extends DialogFragment {
+    private String output;
+
+    public static OnCreateViewDialogFragment newInstance(String output) {
+        OnCreateViewDialogFragment f = new OnCreateViewDialogFragment();
+
+        // Give arguments
+        Bundle args = new Bundle();
+        args.putString("output", output);
+        // Save additional arguments here
+        f.setArguments(args);
+
+        return f;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        output = getArguments().getString("output");
+        // Load any additional arguments here
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.dialog_standard_view, container, false);
+
+        // Set up the TextView
+        TextView outputTV = (TextView) view.findViewById(R.id.output);
+        outputTV.setText(output);
+
+        // Set up the button
+        Button button = (Button) view.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform any other actions here
+                dismiss(); // Without this dismiss, will not go away
+            }
+        });
+
+        // Adding a title
+        //getDialog().setTitle("Title");
+
+        return view;
+    }
+
+    /*
+    // Removing the title
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Dialog d = super.onCreateDialog(savedInstanceState);
+        d.requestWindowFeature(Window.FEATURE_NO_TITLE);    // Remove the bar completely
+        return d;
+    }
+    */
+}
